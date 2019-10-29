542507da92e721ea1bbd4bfb87e44e82cfd9a9e3
BlakeAnd <mblakeandrew@gmail.com>
2019-10-29 14:56:43 -0700

    added functionality without collisions, strted to add collisions

--- src/hashtable.py
+++ src/hashtable.py
@@ -51,7 +51,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        if self.storage[index] is None:
+            new_list = LinkedList({key: value})
+            self.storage[index] = new_list 
+        else:
+            current_list = self.storage[index]
+            current_list.add_to_head({key: value})            
+        print(f"insertion: {self.storage}")
 
 
 
@@ -63,7 +70,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        if self.storage[index] != None:
+            self.storage[index] = None
+        else:
+            print("this key empty, yeet")
+        print(f"removal: {self.storage}")
 
 
     def retrieve(self, key):
@@ -74,7 +86,13 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        print(f"retreival: {index} is {self.storage}")
+        if self.storage[index] is None:
+            return None
+        else:
+            return self.storage[index]
+        
 
 
     def resize(self):
@@ -84,12 +102,69 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        array = []
+        for i in range(0, self.capacity):
+            array.append(self.storage[i])
+        self.capacity *= 2
+        self.storage = [None] * self.capacity  
+        for i in range(0, len(array) - 1):
+            self.storage[i] = array[i]
+        print("resize", self.storage)
 
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next = next_node
+        
+class LinkedList:
+    def __init__(self, set_head=None):
+        self.head = set_head
 
+    def add_to_head(self, value):
+        # Create a head node
+        new_node = Node(value, self.head)
+        # Set current head to new node
+        self.head = new_node
+
+    def remove(self, value):
+        '''
+        Find and remove the node with the given value
+        '''
+        # If we have no head
+        if not self.head:
+            # print an error and return
+            print("Error: Value not found")
+        # If the head has our value
+        elif self.head.value == value:
+            # Remove the head by pointing self.head to head.next
+            self.head = self.head.next
+        # Else
+        else:
+            # Keep track of the parent node
+            parent = self.head
+            current = self.head.next
+            # Walk through the linked list until we find a matching value
+            while current:
+                # If we find a matching value
+                if current.value == value:
+                    # Delete the node by pointing parent.next to node.next
+                    parent.next = current.next
+                    return
+                current = current.next
+            # If we get to the end and have not found the value, print error
+            print("Error: Value not found")
+
+    def contains(self, value):
+        '''
+        Return true if our LL contains the value
+        '''
+		# Fill this in
+        #        
 
 if __name__ == "__main__":
-    ht = HashTable(2)
+    ht = HashTable(3)
+
+    
 
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
@@ -113,5 +188,7 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
+
+    ht.resize()
 
     print("")
